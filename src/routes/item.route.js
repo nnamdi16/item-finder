dbefdd7bc17b26e1dab229713187aa036b3e96c7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2021-01-07 11:48:55 +0100

    created route to search sugggested key word and search byt style name

--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import {searchItemByKeyword} from '../controller/item.controller'
+import {searchItemByKeyword, searchItemByStyleName, searchSuggestedKeywords} from '../controller/item.controller'
 
 const router = express.Router();
-
+router.get('/', (req,res) => res.send("Product Search App loading"));
 router.get('/:marketplace/:language/:searchTerms', searchItemByKeyword);
+router.get('/keywords/:marketplace/:language/:searchTerms', searchSuggestedKeywords);
+router.get('/style/:slug/:styleNumber/:colorCode',searchItemByStyleName )
+
 
 export default router;
