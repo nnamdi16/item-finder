fe66397f0ed7a068372ec76006d81a904879c5ae
nnamdi16 <nwabuokeinnamdi@gmail.com>
2021-01-07 10:45:22 +0100

    modified the callback function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import routes from './routes/item.route';
+import router from './routes/item.route';
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
-app.use('/api', routes);
+app.use('/api', router);
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => 
     console.log(`Listening on port ${port}`));
