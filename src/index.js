29024f1581633fe7cfc4e980f9a6b3f1bedccdc7
Nnamdi <nnwabuokei@paga.com>
2021-01-07 18:24:46 +0100

    removed the route to create an instance

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import router from './routes/item.route';
-const app = express();
+const ItemMonitor = require("./services/ItemMonitor"); 
 
+const itemMonitorInstance = new ItemMonitor();
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(cookieParser());
-app.use('/api', router);
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => 
-    console.log(`Listening on port ${port}`));
-
-module.exports = server;
+console.log(itemMonitorInstance.findItemsByKeywords({marketplace:"us",language:"en", searchTerms:"jordan"}));
